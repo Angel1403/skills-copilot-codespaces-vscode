3672332d171498a16967645230526c7b96b9a192
Angel1403 <79290109+Angel1403@users.noreply.github.com>
2023-11-02 22:55:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello, World!</h1>');
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
